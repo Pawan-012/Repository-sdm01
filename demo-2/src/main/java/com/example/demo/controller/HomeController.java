7aa15f08d9330f338ef3fb99bdddc0e3abe3a410
pawan <patpawa3@gmail.com>
2023-10-15 12:59:51 +0530

    New Addition

---
+++ demo-2/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="*")
+public class HomeController {
+	@GetMapping("/meth01")
+	public String meth() {
+		return "Hello";
+	}
+}
